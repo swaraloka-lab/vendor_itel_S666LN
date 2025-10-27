f21211f6cd8d9261a0eb0a08d80ed5591e8299aa
KimelaZX <mmikailrei@gmail.com>
2025-10-27 21:41:25 +0700

    S666LN: Sync with device tree
    Signed-off-by: KimelaZX <mmikailrei@gmail.com>

--- S666LN-vendor.mk
+++ S666LN-vendor.mk
@@ -937,6 +937,7 @@ PRODUCT_PACKAGES += \
     libmtkcutils \
     libmtkproperty \
     libmtkrillog \
+	libthha \
     libvcodec_oal \
     libvcodecdrv_header_stub \
     libverno \
@@ -958,7 +959,6 @@ PRODUCT_PACKAGES += \
     libmp4enc_xa.ca7 \
     librcs_interface \
     librcs_volte_core \
-    libthha \
     libvcodec_utility_plat.mt6789 \
     libvolte_core_shr \
     libvolte_xdmc_shr \
